349f913f84334b4489d2d977d81f35070bad5ff1
Alexander <alex17.05.84@yandex.ru>
2022-10-26 18:27:12 +0300

    Fourth Task

---
+++ FourthTask/Program4.cs
@@ -0,0 +1,18 @@
+﻿double GetDistance(double x1, double x2,double y1, double y2)
+{
+    double distance = Math.Sqrt(Math.Pow((x2 - x1),2) + Math.Pow((y2 - y1),2));
+    return distance;
+}
+
+double Getnumber(string name)
+{
+    Console.WriteLine("Введите координату " + name);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+double x1 = Getnumber("x1");
+double x2 = Getnumber("x2");
+double y1 = Getnumber("y1");
+double y2 = Getnumber("y2"); 
+
+Console.WriteLine("Расстояние между точками = "+ GetDistance(x1, x2, y1, y2));
